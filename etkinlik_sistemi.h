78e304f943fbabde8c534ca8ab9bdb9d67ceac31
Mowassir-Noor <146969351+Mowassir-Noor@users.noreply.github.com>
2024-01-02 12:30:37 +0300

    Update etkinlik_sistemi.h

--- etkinlik_sistemi.h
+++ etkinlik_sistemi.h
@@ -2,19 +2,30 @@
 #define ETKINLIK_SISTEMI_H
 
 struct Katilimci {
-    char isim[50];
-    char soyisim[50];
-    char email[100];
+
+  char isim[50];
+  char soyisim[50];
+  char email[100];
 };
 
-struct EtkinlikKayitSistemi {
-    struct Katilimci katilimcilar[100];
-    int katilimci_sayisi;
-};
+// struct EtkinlikKayitSistemi {
+  // is there any need of using struct here
+  // struct Katilimci katilimcilar;
+  // int katilimci_sayisi;
+// };
 
-void veriyi_kaydet(struct EtkinlikKayitSistemi* sistem);
-void kayit_ekle(struct EtkinlikKayitSistemi* sistem, const char* isim, const char* soyisim, const char* email);
-void katilimci_listesi_goruntule(const struct EtkinlikKayitSistemi* sistem);
+void veriyi_kaydet(const struct Katilimci *sistem);
+void kayit_ekle(struct Katilimci *sistem, const char *isim,
+                const char *soyisim, const char *email);
+void kayit_iptal(struct EtkinlikKayitSistemi *sistem, const char *isim,
+                 const char *soyisim);
+void katilimci_listesi_goruntule(const struct EtkinlikKayitSistemi *sistem);
 void show_menu();
+// extras
+void numarayla_silme(struct Katilimci *sistem, int delete_line);
+void show();
+int total_lines();
+void main_menu();
 
+void main_veriyi_kaydet();
 #endif /* ETKINLIK_SISTEMI_H */
