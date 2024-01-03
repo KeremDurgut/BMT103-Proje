50b294f3b0361b625fbd36fbb21290aeb3518062
Mowassir-Noor <146969351+Mowassir-Noor@users.noreply.github.com>
2024-01-03 11:13:34 +0300

    Update main.c

--- main.c
+++ main.c
@@ -11,98 +11,100 @@ int main() {
     getchar(); // Clear the newline from the buffer
 
     switch (choice) {
-      case 1:
-        etkinlik_ekleme();
-        break;
+    case 1:
+      etkinlik_ekleme();
+      break;
 
-      case 2: {
-        char etkinlik_listesi[30]="etkinlik_listesi.txt";
-        int islem;
-        etkinlik_liste_goruntuleme();
-        printf("Hangi etkinlikle işlem yapmak istiyorsunuz: ");
-        scanf("%d", &islem);
-        const char *filename = "etkinlik_listesi.txt";
-        int lineNumber = islem;
-        char lineBuffer[256];
+    case 2: {
+      char etkinlik_listesi[30] = "etkinlik_listesi.txt";
+      int islem;
+      etkinlik_liste_goruntuleme();
+      printf("Hangi etkinlikle işlem yapmak istiyorsunuz: ");
+      scanf("%d", &islem);
+      const char *filename = "etkinlik_listesi.txt";
+      int lineNumber = islem;
+      char file[256];
 
-        if (copy_line_to_variable(filename, lineNumber, lineBuffer)) {
-          printf("Line %d: %s", lineNumber, lineBuffer);
-        } else {
-          printf("Line %d not found or file couldn't be opened.\n", lineNumber);
-        }
-
-        etkinlik_sil(lineBuffer);
-        numarayla_silme(lineNumber,etkinlik_listesi );
-        break;
+      if (copy_line_to_variable(filename, lineNumber, file)) { 
+        printf("Line %d: %s", lineNumber,file);
+      } else {
+        printf("Line %d not found or file couldn't be opened.\n", lineNumber);
       }
 
-      case 3: {
-        int islem;
-        etkinlik_liste_goruntuleme();
-        printf("Hangi etkinlikle işlem yapmak istiyorsunuz: ");
-        scanf("%d", &islem);
-        const char *filename = "etkinlik_listesi.txt";
-        int lineNumber = islem;
-        char lineBuffer[256];
+      etkinlik_sil(file);
 
-        if (copy_line_to_variable(filename, lineNumber, lineBuffer)) {
-          printf("Line %d: %s", lineNumber, lineBuffer);
-        } else {
-          printf("Line %d not found or file couldn't be opened.\n", lineNumber);
-        }
-        int secim;
-        do {
-          show_menu(lineBuffer);
-          // int secim;
-          scanf("%d", &secim);
+      // we have to edit this
+      delete_line(etkinlik_listesi, lineNumber);
+      break;
+    }
 
-          switch (secim) {
-            case 1: {
-              char isim[50], soyisim[50], email[100];
-              printf("Enter Name: ");
-              scanf("%49s", isim);
-              printf("Enter Surname: ");
-              scanf("%49s", soyisim);
-              printf("Enter Email: ");
-              scanf("%99s", email);
-              kayit_ekle(&etkinlik_sistemi, isim, soyisim, email, lineBuffer);
-              break;
-            }
+    case 3: {
+      int islem;
+      etkinlik_liste_goruntuleme();
+      printf("Hangi etkinlikle işlem yapmak istiyorsunuz: ");
+      scanf("%d", &islem);
+      const char *filename = "etkinlik_listesi.txt";
+      int lineNumber = islem;
+      char file[256];
 
-            case 2: {
-              int line_to_delete;
-              do {
-                printf("Write the line number that you want to delete: ");
-                printf("Menu'ye geri dönmek için 0: ");
-                scanf("%d", &line_to_delete);
-
-                numarayla_silme(line_to_delete, lineBuffer);
-              } while (line_to_delete != 0);
-
-              break;
-            }
-
-            case 3:
-              show(lineBuffer);
-              break;
-
-            default:
-              printf("Invalid choice. Please choose again.\n");
-              break;
-          }
-
-        } while (secim != 0);
-
-        break;
+      if (copy_line_to_variable(filename, lineNumber, file)) {
+        printf("Line %d: %s", lineNumber, file);
+      } else {
+        printf("Line %d not found or file couldn't be opened.\n", lineNumber);
       }
+      int secim;
+      do {
+        show_menu(file);
+        // int secim;
+        scanf("%d", &secim);
 
-      case 0:
-        printf("Exiting...\n");
-        break;
+        switch (secim) {
+        case 1: {
+          char isim[50], soyisim[50], email[100];
+          printf("Enter Name: ");
+          scanf("%49s", isim);
+          printf("Enter Surname: ");
+          scanf("%49s", soyisim);
+          printf("Enter Email: ");
+          scanf("%99s", email);
+          kayit_ekle(&etkinlik_sistemi, isim, soyisim, email, file);
+          break;
+        }
 
-      default:
-        printf("Invalid choice. Please choose again.\n");
-        break;
+        case 2: {
+          int line_to_delete;
+          do {
+            printf("Write the line number that you want to delete: ");
+            printf("Menu'ye geri dönmek için 0: ");
+            scanf("%d", &line_to_delete);
+
+            numarayla_silme(line_to_delete, file);
+          } while (line_to_delete != 0);
+
+          break;
+        }
+
+        case 3:
+          show_file(file);
+          break;
+
+        default:
+          printf("Invalid choice. Please choose again.\n");
+          break;
+        }
+
+      } while (secim != 0);
+
+      break;
+    }
+
+    case 0:
+      printf("Exiting...\n");
+      break;
+
+    default:
+      printf("Invalid choice. Please choose again.\n");
+      break;
     }
   } while (choice != 0);
 
